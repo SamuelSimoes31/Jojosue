da646dc05f7ea8dcb4bfb999c95aed583d9e3bb5
Samuel Simoes de Souza Filho <sssf2@ug1c35.windows.cin.ufpe.br>
2019-06-13 10:18:37 -0300

    Boneco andando com mapa acomhnhado e no centro da tela
    foi isto

--- testes/Teste_impressao_mapa/madeeasy.c
+++ testes/Teste_impressao_mapa/madeeasy.c
@@ -5,8 +5,21 @@
 #include <allegro5/allegro_image.h>
 #include<stdio.h>
 
-#define LARGURA_TELA 800
-#define ALTURA_TELA 600
+#define LARGURA_TELA 1600
+#define ALTURA_TELA 900
+
+
+float cameraPosition[2] = {0,0}, scale = 1.0;
+void cameraUpdate(float* cameraPosition, float x, float y, int width, int height ){
+    
+    cameraPosition[0] = -(LARGURA_TELA/2/scale) + (x + width/2);
+    cameraPosition[1] = -(ALTURA_TELA/2/scale) + (y + height/2);
+    if(cameraPosition[0] < 0) cameraPosition[0] = 0;
+    if(cameraPosition[1] < 0) cameraPosition[1] = 0;
+    //if(cameraPosition[0] > ALTURA_TELA); cameraPosition[0] = ALTURA_TELA;
+    //if(cameraPosition[1] > LARGURA_TELA); cameraPosition[1] = ALTURA_TELA;
+    printf("x=%g y=%g cameraPosition[0]=%g cameraPosition[1]=%g scale=%g \n",x,y,cameraPosition[0],cameraPosition[1],scale);
+}
 
 int main()
 {
@@ -15,7 +28,7 @@ int main()
     enum direction {DOWN, UP, LEFT, RIGHT};
 
     const float FPS = 60.0;
-    const float frameFPS = 7.5;
+    const float frameFPS = 15;
 
     if(!al_init())
         puts("Fala ao iniciar allegro\n");
@@ -27,9 +40,10 @@ int main()
 
     al_set_window_position(display,200,200);
 
-    int done = false, draw = true, active = false;
-    float x = 10, y = 10, moveSpeed = 2.5;
+    bool done = false, draw = true, active = false;
+    float x = 10, y = 10, moveSpeed = 4;
     int dir = DOWN, sourceX = 32, sourceY = 0;
+    
 
     al_install_keyboard();
     al_init_image_addon();
@@ -38,6 +52,8 @@ int main()
     ALLEGRO_BITMAP *background = al_load_bitmap("mapadef.png");
 
     ALLEGRO_KEYBOARD_STATE keyState;
+
+    ALLEGRO_TRANSFORM camera;
 
     ALLEGRO_TIMER *timer = al_create_timer(1.0/FPS);
     ALLEGRO_TIMER *frameTimer = al_create_timer(1.0/frameFPS);
@@ -86,25 +102,36 @@ int main()
                     x -= moveSpeed;
                     dir = LEFT;
                 }
-                else if(al_key_down(&keyState, ALLEGRO_KEY_ESCAPE))
-                {
-                done = true;
-                }
                 else{
                     active = false;
-                    }
-                //printf("active:%d",active);
+                }
+
+                if(al_key_down(&keyState, ALLEGRO_KEY_ESCAPE))
+                {
+                    done = true;
+                }
+                else if(al_key_down(&keyState, ALLEGRO_KEY_Z))
+                {
+                    scale += 0.1;
+                }
+                else if(al_key_down(&keyState, ALLEGRO_KEY_X))
+                {
+                    scale -= 0.1;
+                }
                 
+                cameraUpdate(cameraPosition,x,y,32,32);
+
+                al_identity_transform(&camera);
+                al_translate_transform(&camera, -cameraPosition[0],-cameraPosition[1]);
+                al_scale_transform(&camera,scale,scale);
+                al_use_transform(&camera);
 
             }
 
             else if(events.timer.source == frameTimer)
             {
                 if(active){
-                    //printf("if sourceX antes = %d \n",sourceX);
-                    int opa = sourceX; 
-                    //sourceX += al_get_bitmap_width(player) / 4;
-                    sourceX = opa + 32;
+                    sourceX += al_get_bitmap_width(player) / 4;
                     //printf("if sourceX depois= %d \n",sourceX);
                 }
                 else{
@@ -112,9 +139,7 @@ int main()
                     //printf("else sourceX = %d\n",sourceX);
                 }
 
-                if(sourceX >= al_get_bitmap_width(player))
-                    sourceX = 0;
-
+                if(sourceX >= al_get_bitmap_width(player)) sourceX = 0;
                 sourceY = dir;
                 //printf("sourceX = %d  sourceY = %d\n",sourceX,sourceY);
             }
